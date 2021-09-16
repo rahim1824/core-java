2d5e48570803879b649bffe2efbe5bc9fe18e24b
rahim1824 <88377217+rahim1824@users.noreply.github.com>
2021-09-16 15:50:58 +0530

    Add files via upload

--- Account.java
+++ Account.java
@@ -9,12 +9,22 @@ class Account
     {
         return balance;
     }
+        void withdraw(double amount)
+        {
+		if(amount+1000<=balance)
+			balance=balance-amount;
+ 		else
+			System.out.println("NO sufficient balance");
+	}
     public static void main(String args[])
     {
        Account acc=new Account();
        acc.addBalance(1000);
        acc.addBalance(acc.getBalance()*2);
-       System.out.println(acc.getBalance());
+       System.out.println("before withdraw"+acc.getBalance());
+	acc.withdraw(1000);
+	 System.out.println("after withdraw"+acc.getBalance());
+
     }
     
 }
